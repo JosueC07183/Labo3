ef370864461592fe0d628f2d4695ba14a698bc02
JosueC07183 <josue.salmeron@ucr.ac.cr>
2024-01-27 23:11:00 -0600

    Medición de tensiones AC y alarmas

--- src/VOlTMETER/VOlTMETER.ino
+++ src/VOlTMETER/VOlTMETER.ino
@@ -1,41 +1,29 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
-
 #define CLK 7
 #define DIN 6
 #define DC  5
 #define CS  4
 #define RST 3
-#define CH_1 8
-#define CH_2 9
-#define CH_3 10
-#define CH_4 11
-
+// Configuración de los pines de la pantalla.
 Adafruit_PCD8544 display = Adafruit_PCD8544(CLK, DIN, DC, CS,RST); // Esto debe ir en orden.
-
 // Variables LEDs alarma
 const int LED_A3 = 8;
 const int LED_A2 = 9;
 const int LED_A1 = 10;
 const int LED_A0 = 11;
  // Voltajes en DC
- float v_1=0.0;
- float v_2=0.0;
- float v_3=0.0;
- float v_4 =0.0;
- // Voltajes en AC
- float va_1=0.0;
- float va_2=0.0;
- float va_3=0.0;
- float va_4 =0.0;
- 
-
+static float volt_1=0.0;
+static float volt_2=0.0;
+static float volt_3=0.0;
+static float volt_4 =0.0;
+// Configuraciones generales
 void setup() { 
   // Canales de voltaje
-  pinMode(CH_1, INPUT);
-  pinMode(CH_2, INPUT);
-  pinMode(CH_3, INPUT);
-  pinMode(CH_4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A2, INPUT);
   // LEDs de alarma.
   pinMode(LED_A3, OUTPUT);
   pinMode(LED_A2, OUTPUT);
@@ -54,7 +42,6 @@ void setup() {
   display.clearDisplay(); // Clear display
 
 }
-
 // Funcion medir voltaje
 float mide_voltaje(float mag) {
   float max_val = 0;
@@ -68,66 +55,114 @@ float mide_voltaje(float mag) {
     max_val = (((511.0-max_val)* 48/1023.0)); // normalizar y escalar.
     return max_val;
 }
-
 // Funcion encargada de indicar sobrepaso con leds
 void alarma_DC(int medida1, int medida2, int medida3, int medida4) {
   // Primer LED
-  if(-20 > medida1 || medida1 > 20){
+  if(-20 >= medida1 || medida1 >= 20){
     digitalWrite(LED_A3, HIGH);
   }
   else{
     digitalWrite(LED_A3, LOW);
   }
   // Segundo LED
-  if(-20 > medida2 || medida2 > 20){
+  if(-20 >= medida2 || medida2 >= 20){
     digitalWrite(LED_A2, HIGH);
   }
   else{
     digitalWrite(LED_A2, LOW);
   }
   // Tercer LED
-  if(-20 > medida3 || medida3 > 20){
+  if(-20 >= medida3 || medida3 >= 20){
     digitalWrite(LED_A1, HIGH);
   }
   else{
     digitalWrite(LED_A1, LOW);
   }
   // Cuarto LED
-  if(-20 > medida4 || medida4 > 20){
+  if(-20 >= medida4 || medida4 >= 20){
     digitalWrite(LED_A0, HIGH);
   }
   else{
     digitalWrite(LED_A0, LOW);
   }
 }
+// Alarma AC
+void alarma_AC(float medida1, float medida2, float medida3, float medida4){
+  if(-14.14 >= medida1 || medida1 >= 14.14){
+    digitalWrite(LED_A3, HIGH);
+  }
+  else{
+    digitalWrite(LED_A3, LOW);
+  }
+  // Segundo LED
+  if(-14.14 >= medida2 || medida2 >= 14.14){
+    digitalWrite(LED_A2, HIGH);
+  }
+  else{
+    digitalWrite(LED_A2, LOW);
+  }
+  // Tercer LED
+  if(-14.14 >= medida3 || medida3 >= 14.14){
+    digitalWrite(LED_A1, HIGH);
+  }
+  else{
+    digitalWrite(LED_A1, LOW);
+  }
+  // Cuarto LED
+  if(-14.14 >= medida4 || medida4 >= 14.14){
+    digitalWrite(LED_A0, HIGH);
+  }
+  else{
+    digitalWrite(LED_A0, LOW);
+  }
+}
+// Mostrar valores en pantalla
+void tensiones_DC(){
+  display.print("V1: ");display.print(volt_1);display.print("V");
+  display.print("\n");
+  display.print("V2: ");display.print(volt_2);display.print("V");
+  display.print("\n");
+  display.print("V3: ");display.print(volt_3);display.print("V");
+  display.print("\n");
+  display.print("V4: ");display.print(volt_4);display.print("V");
+  display.display();
+  display.clearDisplay();
+}
+void tensiones_AC(){
+  display.print("V1: ");display.print(volt_1);display.print(" rms");
+  display.print("\n");
+  display.print("V2: ");display.print(volt_2);display.print(" rms");
+  display.print("\n");
+  display.print("V3: ");display.print(volt_3);display.print(" rms");
+  display.print("\n");
+  display.print("V4: ");display.print(volt_4);display.print(" rms");
+  display.display();
+  display.clearDisplay();
+}
 
 // Loop principal donde se ejecutan las funciones del voltimetro.
-
 void loop() {
-  float volt_1 = mide_voltaje(A5);
-  float volt_2 = mide_voltaje(A4);
-  float volt_3 = mide_voltaje(A3);
-  float volt_4 = mide_voltaje(A2);
-  display.clearDisplay();
-
-  // Revisar si algunos de los valores superaron +20V o son menores a -20V
-  alarma_DC(volt_1, volt_2, volt_3, volt_4);
-
-  display.setCursor(0, 0);
-  display.print("V1: ");
-  display.println(volt_1, 2); // Mostrar el valor con 2 decimales
-
-  display.setCursor(0, 10);
-  display.print("V2: ");
-  display.println(volt_2, 2);
-
-  display.setCursor(0, 20);
-  display.print("V3: ");
-  display.println(volt_3, 2);
-
-  display.setCursor(0, 30);
-  display.print("V4: ");
-  display.println(volt_4, 2);
-
-  display.display();
-}
+    float mode = analogRead(A0); // Lectura de voltajes en AC
+    if (mode){
+      display.print("Modo AC \n");
+      volt_1 = mide_voltaje(A5);
+      volt_1 = (volt_1/sqrt(2)); // Conversión a rms
+      volt_2 = mide_voltaje(A4);
+      volt_2 = (volt_2/sqrt(2)); // Conversión a rms
+      volt_3 = mide_voltaje(A3);
+      volt_3 = (volt_3/sqrt(2)); // Conversión a rms
+      volt_4 = mide_voltaje(A2); 
+      volt_4 = (volt_4/sqrt(2)); // Conversión a rms
+      tensiones_AC(); // Muestra magnitudes en la pantalla
+      alarma_AC(volt_1, volt_2, volt_3, volt_4); // Alarma de sobre-tensiones
+    }
+    else{
+      display.print("Modo DC \n"); // Si el botón está en bajo, se muestran las tensiones en DC
+      volt_1 = mide_voltaje(A5);
+      volt_2 = mide_voltaje(A4);
+      volt_3 = mide_voltaje(A3);
+      volt_4 = mide_voltaje(A2);  
+      tensiones_DC(); // Muestra magnitudes en la pantalla
+      alarma_DC(volt_1, volt_2, volt_3, volt_4); // Alarma de los LEDs de sobre-voltaje.
+    }
+  }
