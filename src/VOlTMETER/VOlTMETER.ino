c9f2a64c627112ccf2d0234b4866de6a75dcc48a
KevinCamposC <136384525+KevinCamposC@users.noreply.github.com>
2024-01-28 17:57:18 -0800

    Hex y codigo actualizado.
    Se implementa la comunicacion en el codigo para que se pueda comunicar con un puerto virtual, ademas se actualiza el hex. Se comprobo la funcionalidad de todo.

--- src/VOlTMETER/VOlTMETER.ino
+++ src/VOlTMETER/VOlTMETER.ino
@@ -1,10 +1,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
+#include <SPI.h>
+
 #define CLK 7
 #define DIN 6
 #define DC  5
 #define CS  4
 #define RST 3
+
 // Configuración de los pines de la pantalla.
 Adafruit_PCD8544 display = Adafruit_PCD8544(CLK, DIN, DC, CS,RST); // Esto debe ir en orden.
 // Variables LEDs alarma
@@ -13,12 +16,14 @@ const int LED_A2 = 9;
 const int LED_A1 = 10;
 const int LED_A0 = 11;
  // Voltajes en DC
-static float volt_1=0.0;
-static float volt_2=0.0;
-static float volt_3=0.0;
-static float volt_4 =0.0;
+static float volt_1 = 0.0;
+static float volt_2 = 0.0;
+static float volt_3 = 0.0;
+static float volt_4 = 0.0;
 // Configuraciones generales
 void setup() { 
+  //Comunicacion serial
+  Serial.begin(9600);
   // Canales de voltaje
   pinMode(A5, INPUT);
   pinMode(A4, INPUT);
@@ -40,8 +45,8 @@ void setup() {
   display.display();
   delay(2000);
   display.clearDisplay(); // Clear display
-
 }
+
 // Funcion medir voltaje
 float mide_voltaje(float mag) {
   float max_val = 0;
@@ -55,8 +60,9 @@ float mide_voltaje(float mag) {
     max_val = (((511.0-max_val)* 48/1023.0)); // normalizar y escalar.
     return max_val;
 }
+
 // Funcion encargada de indicar sobrepaso con leds
-void alarma_DC(int medida1, int medida2, int medida3, int medida4) {
+void alarma_DC(float medida1, float medida2, float medida3, float medida4) {
   // Primer LED
   if(-20 >= medida1 || medida1 >= 20){
     digitalWrite(LED_A3, HIGH);
@@ -86,6 +92,7 @@ void alarma_DC(int medida1, int medida2, int medida3, int medida4) {
     digitalWrite(LED_A0, LOW);
   }
 }
+
 // Alarma AC
 void alarma_AC(float medida1, float medida2, float medida3, float medida4){
   if(-14.14 >= medida1 || medida1 >= 14.14){
@@ -116,6 +123,7 @@ void alarma_AC(float medida1, float medida2, float medida3, float medida4){
     digitalWrite(LED_A0, LOW);
   }
 }
+
 // Mostrar valores en pantalla
 void tensiones_DC(){
   display.print("V1: ");display.print(volt_1);display.print("V");
@@ -128,6 +136,7 @@ void tensiones_DC(){
   display.display();
   display.clearDisplay();
 }
+
 void tensiones_AC(){
   display.print("V1: ");display.print(volt_1);display.print(" rms");
   display.print("\n");
@@ -138,6 +147,32 @@ void tensiones_AC(){
   display.print("V4: ");display.print(volt_4);display.print(" rms");
   display.display();
   display.clearDisplay();
+}
+
+// Funcion encargada de la comunicacion
+void comunicacion(float medida1, float medida2, float medida3, float medida4, float mode){
+  if (mode){
+      // Comunicacion serial AC
+      Serial.print("Voltaje canal 1 (AC): ");
+      Serial.println(volt_1);
+      Serial.print("Voltaje canal 2 (AC): ");
+      Serial.println(volt_2);
+      Serial.print("Voltaje canal 3 (AC): ");
+      Serial.println(volt_3);
+      Serial.print("Voltaje canal 4 (AC): ");
+      Serial.println(volt_4);
+  }
+  else{
+      // Comunicacion serial DC
+      Serial.print("Voltaje canal 1 (DC): ");
+      Serial.println(volt_1);
+      Serial.print("Voltaje canal 2 (DC): ");
+      Serial.println(volt_2);
+      Serial.print("Voltaje canal 3 (DC): ");
+      Serial.println(volt_3);
+      Serial.print("Voltaje canal 4 (DC): ");
+      Serial.println(volt_4);
+  }
 }
 
 // Loop principal donde se ejecutan las funciones del voltimetro.
@@ -155,6 +190,7 @@ void loop() {
       volt_4 = (volt_4/sqrt(2)); // Conversión a rms
       tensiones_AC(); // Muestra magnitudes en la pantalla
       alarma_AC(volt_1, volt_2, volt_3, volt_4); // Alarma de sobre-tensiones
+      comunicacion(volt_1, volt_2, volt_3, volt_4, mode);
     }
     else{
       display.print("Modo DC \n"); // Si el botón está en bajo, se muestran las tensiones en DC
@@ -164,5 +200,6 @@ void loop() {
       volt_4 = mide_voltaje(A2);  
       tensiones_DC(); // Muestra magnitudes en la pantalla
       alarma_DC(volt_1, volt_2, volt_3, volt_4); // Alarma de los LEDs de sobre-voltaje.
+      comunicacion(volt_1, volt_2, volt_3, volt_4, mode);
     }
   }
